415dec3a519aa406207e283004efbbcc7945b448
Your Name <you@example.com>
2026-06-01 14:41:55 +1000

    feat: capacity to send notifications.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use notify_rust::Notification;
 fn main() {
     println!("Hello, world!");
+    Notification::new()
+        .summary("Firefox News")
+        .body("This will almost look like a real firefox notification.")
+        .icon("firefox")
+        .show()
+        .expect("failed to send notification");
 }
