749a731eec31d5533b46d20ea0fde126ff972724
Your Name <you@example.com>
2026-06-01 18:49:21 +1000

    feat: The basic noise + notifications + args done

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,62 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use notify_rust::Notification;
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
-
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    #[command(subcommand)]
+    command: Commands,
 }
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Run a simple Pomodoro timer.
+    Run {
+        #[arg(default_value = "50m")]
+        study: String,
+        #[arg(default_value = "10m")]
+        rest: String,
+    },
+    /// Connect to a peer (not implemented)
+    Connect {
+        // #[arg(short)]
+        name: String,
+    },
+}
+fn play_noise(noise: &str) {
+    use std::fs::File;
+    use std::io::BufReader;
+    let mut sink_handle =
+        rodio::DeviceSinkBuilder::open_default_sink().expect("open default audio stream");
+    sink_handle.log_on_drop(false);
+    let file = BufReader::new(File::open("assets/sfx/".to_string() + noise).unwrap());
+    let player = rodio::play(&sink_handle.mixer(), file).unwrap();
+    player.sleep_until_end();
+}
+fn send_notification(summary: &str, body: &str) {
     Notification::new()
-        .summary("Firefox News")
-        .body("This will almost look like a real firefox notification.")
-        .icon("firefox")
+        .summary(summary)
+        .body(body)
         .timeout(5000)
         .show()
         .expect("failed to send notification");
+}
+
+fn main() {
+    // let duration = humantime::parse_duration("50s").unwrap();
+    // println!("{}", duration.as_millis());
     let args = Args::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name);
+    match args.command {
+        Commands::Run { study, rest } => {
+            use humantime::parse_duration;
+            println!("Hello, world!");
+            println!("{:?}, {}", parse_duration(&study).unwrap().as_secs(), rest);
+            send_notification("Study Finished", &format!("Your study timer of {} is finished", study));
+            play_noise("POMODORO-FINISH.wav");
+            send_notification("Break Finished", &format!("Your rest timer of {} is finished", rest));
+            play_noise("POMODORO-BREAK-END.wav");
+        }
+        Commands::Connect { .. } => {}
     }
 }
