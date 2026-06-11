feb94eb8e74d2e6195ca24d34e6c43595f1adb72
Your Name <you@example.com>
2026-06-11 20:23:40 +1000

    feat: WE ARE USING TAURI NOW

--- flake.nix
+++ flake.nix
@@ -1,22 +1,13 @@
 {
-  description = "Build a cargo project";
+  description = "Tauri v2 app";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
     crane.url = "github:ipetkov/crane";
-
+    # crane-tauri declares no inputs of its own, so there is nothing to follow
+    # or override (the previous follows clause warned about non-existent inputs).
+    crane-tauri.url = "github:JPHutchins/crane-tauri";
     flake-utils.url = "github:numtide/flake-utils";
-
-    advisory-db = {
-      url = "github:rustsec/advisory-db";
-      flake = false;
-    };
-
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
@@ -24,178 +15,74 @@
       self,
       nixpkgs,
       crane,
+      crane-tauri,
       flake-utils,
-      advisory-db,
-      rust-overlay,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
+        pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs) lib;
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (import rust-overlay) ];
-        };
+        craneLib = crane.mkLib pkgs;
 
-        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-          targets = [ "x86_64-pc-windows-msvc" ];
-        };
+        frontend = pkgs.buildNpmPackage {
+          pname = "my-app-frontend"; # TODO: change
+          version = "0.1.0"; # TODO: change
 
-        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
-        src =
-          let
-            unfilteredRoot = ./.;
-          in
-          lib.fileset.toSource {
-            root = unfilteredRoot;
+          src = lib.fileset.toSource {
+            root = ./.;
             fileset = lib.fileset.unions [
-              # Default files from crane (Rust and cargo files)
-              (craneLib.fileset.commonCargoSources unfilteredRoot)
-              # Also keep any markdown files
-              (lib.fileset.fileFilter (file: file.hasExt "md") unfilteredRoot)
-              # Example of a folder for images, icons, etc
-              (lib.fileset.maybeMissing ./assets)
+              ./package.json
+              ./package-lock.json
+              ./tsconfig.json
+              ./tsconfig.node.json
+              ./vite.config.ts
+              ./index.html
+              ./src
+              ./public
             ];
           };
 
-        # Common arguments can be set here to avoid repeating them later
-        commonArgs = {
-          inherit src;
-          strictDeps = true;
+          npmDepsHash = ""; # TODO: build once to get correct hash
 
-          nativeBuildInputs = [
-            pkgs.pkg-config
-          ];
-
-          buildInputs = [
-            pkgs.alsa-lib
-          ]
-          ++ lib.optionals pkgs.stdenv.isDarwin [
-            # Additional darwin specific inputs can be set here
-            pkgs.libiconv
-          ];
-
-          # Additional environment variables can be set directly
-          # MY_CUSTOM_VAR = "some value";
-          # RUST_MIN_STACK = 134217728; # NOTE: good luck compiling this xDD
-          CARGO_BUILD_JOBS = 2; # NOTE: yeah heres the fix from the constant OOM
-          # I've set it to 2, fuck off kernel panic
+          installPhase = ''
+            runHook preInstall
+            cp -r dist $out
+            runHook postInstall
+          '';
         };
 
-        # Build *just* the cargo dependencies, so we can reuse
-        # all of that work (e.g. via cachix) when running in CI
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-
-        # Build the actual crate itself, reusing the dependency
-        # artifacts from above.
-        my-crate = craneLib.buildPackage (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-          }
-        );
+        tauri = crane-tauri.lib.buildTauriApp { inherit pkgs craneLib; } {
+          pname = "my-app"; # TODO: change
+          version = "0.1.0"; # TODO: change
+          src = ./.;
+          inherit frontend;
+        };
       in
       {
-        checks = {
-          # Build the crate as part of `nix flake check` for convenience
-          inherit my-crate;
-
-          # Run clippy (and deny all warnings) on the crate source,
-          # again, reusing the dependency artifacts from above.
-          #
-          # Note that this is done as a separate derivation so that
-          # we can block the CI if there are issues here, but not
-          # prevent downstream consumers from building our crate by itself.
-          my-crate-clippy = craneLib.cargoClippy (
-            commonArgs
-            // {
-              inherit cargoArtifacts;
-              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-            }
-          );
-
-          my-crate-doc = craneLib.cargoDoc (
-            commonArgs
-            // {
-              inherit cargoArtifacts;
-              # This can be commented out or tweaked as necessary, e.g. set to
-              # `--deny rustdoc::broken-intra-doc-links` to only enforce that lint
-              env.RUSTDOCFLAGS = "--deny warnings";
-            }
-          );
-
-          # Check formatting
-          my-crate-fmt = craneLib.cargoFmt {
-            inherit src;
-          };
-
-          my-crate-toml-fmt = craneLib.taploFmt {
-            src = pkgs.lib.sources.sourceFilesBySuffices src [ ".toml" ];
-            # taplo arguments can be further customized below as needed
-            # taploExtraArgs = "--config ./taplo.toml";
-          };
-
-          # Audit dependencies
-          my-crate-audit = craneLib.cargoAudit {
-            inherit src advisory-db;
-          };
-
-          # Audit licenses
-          my-crate-deny = craneLib.cargoDeny {
-            inherit src;
-          };
-
-          # Run tests with cargo-nextest
-          # Consider setting `doCheck = false` on `my-crate` if you do not want
-          # the tests to run twice
-          my-crate-nextest = craneLib.cargoNextest (
-            commonArgs
-            // {
-              inherit cargoArtifacts;
-              partitions = 1;
-              partitionType = "count";
-              cargoNextestPartitionsExtraArgs = "--no-tests=pass";
-            }
-          );
-        };
-
         packages = {
-          default = my-crate;
+          inherit frontend;
+          default = tauri.app;
         };
 
-        apps.default = flake-utils.lib.mkApp {
-          drv = my-crate;
+        checks = {
+          inherit (tauri) app;
+
+          clippy = craneLib.cargoClippy (
+            tauri.commonArgs
+            // {
+              cargoArtifacts = tauri.cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- -D warnings";
+              TAURI_CONFIG = tauri.tauriConfig;
+            }
+          );
+
+          fmt = craneLib.cargoFmt { src = tauri.commonArgs.src; };
         };
 
-        devShells.pomotimer = pkgs.mkShell {
-          packages = [ my-crate ];
-          shellHook = ''
-            export REPO_ROOT=$(git rev-parse --show-toplevel)
-            export PS1="Pomotimer $"
-            export PS1="\[\e[38;5;141m\]❯\[\e[0m\] "
-            clear
-          '';
-        };
         devShells.default = craneLib.devShell {
-          # Inherit inputs from checks.
           checks = self.checks.${system};
-
-          # Additional dev-shell environment variables can be set directly
-          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
-
-          # Extra inputs can be added here; cargo and rustc are provided by default.
-          packages = [
-          ]
-          ++ (with pkgs; [
-            rustfmt
-            rust-analyzer
-            cargo-xwin
-          ]);
-          shellHook = ''
-            export REPO_ROOT=$(git rev-parse --show-toplevel)
-            export PS1="\n\[\033[1;32m\][nix-shell:\w]\$\[\033[0m\] "
-          '';
         };
       }
     );
